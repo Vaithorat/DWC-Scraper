2cee899ddd86540416da7cf9a0df5b88f6f17f12
Vaithorat <vaibhavthorat141997@gmail.com>
2024-06-22 17:45:45 +0530

    scraper completed

---
+++ src/app/cron.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+async function callApi() {
+  try {
+    const response = await axios.get(
+      "https://your-vercel-app.vercel.app/api/scraper"
+    );
+    console.log("API call successful:", response.data);
+  } catch (error) {
+    console.error("Failed to call API:", error.message);
+  }
+}
+
+module.exports = async function (req, res) {
+  try {
+    await callApi();
+    res.status(200).send("Scheduled function ran successfully");
+  } catch (error) {
+    console.error("Error running scheduled function:", error.message);
+    res.status(500).send("Scheduled function encountered an error");
+  }
+};
---
+++ src/pages/api/scraper.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+import nodemailer from 'nodemailer';
+
+const handler = async (req, res) => {
+  const url = "https://www.delhiwatchcompany.com";
+
+  console.log("Fetching URL:", url);
+
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    console.log("Loaded HTML");
+
+    let emailSent = false;
+
+    $(".FeaturedProduct").each((index, element) => {
+      const name = $(element).find(".ProductMeta__Title a").text().trim();
+      const price = $(element).find(".ProductMeta__Price .money").text().trim();
+      const availability = $(element).find(".ProductForm__AddToCart").text().trim();
+
+      console.log(`Watch: ${name} - Price: ${price} - Availability: ${availability}`);
+
+      if (availability.toLowerCase() !== "sold out" && !emailSent) {
+        sendEmail(name, price, availability);
+        emailSent = true; 
+      }
+    });
+
+    res.status(200).json({ message: 'Scraping completed' });
+  } catch (error) {
+    console.error(`Error fetching the URL: ${error}`);
+    res.status(500).json({ error: 'Failed to fetch the URL' });
+  }
+};
+
+async function sendEmail(name, price, availability) {
+  // Create a Nodemailer transporter using SMTP
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+  port: 465, 
+    secure: true,
+    auth: {
+      user: process.env.FROMEMAIL, 
+      pass: process.env.PASSWORD
+    }
+  });
+
+  // Email content
+  let mailOptions = {
+    from: process.env.FROMEMAIL,
+    to: process.env.TOEMAIL,
+    subject: 'Watch Availability Notification',
+    text: `Watch: ${name}\nPrice: ${price}\nAvailability: ${availability}`
+  };
+
+  // Send email
+  try {
+    let info = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully!', info.response);
+  } catch (error) {
+    console.error('Failed to send email.', error);
+  }
+}
+
+export default handler;
